8707d34f2dbf135b4eb7cd60a1450c276bae23cd
Your Name <you@example.com>
2022-01-29 22:59:50 +0900

    before making slide.js

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,22 +1,12 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import celestialsJson from "../assets/celestials.json";
+import Header from "../components/Header";
 function Home() {
-  const [searching, setSearching] = useState("");
   const celestials = celestialsJson.celestial;
   return (
     <div>
-      <div className="header">
-        <div className="systemNav">
-          <ul className="solarSystem">solarSystem</ul>
-          <ul className="homeGalaxy">homeGalaxy</ul>
-        </div>
-        <select value="searching">
-          {celestials.map((celestial) => (
-            <option value={celestial.name}>{celestial.name}</option>
-          ))}
-        </select>
-      </div>
+      <Header />
     </div>
   );
 }
